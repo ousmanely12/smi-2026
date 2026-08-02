12c745d690f4d2cca74b49fc8f1557e9f7c1a045
Laye-Prog <wadelaye226@gmail.com>
2026-08-02 22:15:02 +0000

    Résolution du conflit et fusion feature/membres-backend

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PotController;
 use App\Http\Controllers\MembreController;
 use App\Http\Controllers\CotisationController;
+use App\Http\Controllers\RegistreController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/auth/request-otp', [AuthController::class, 'requestOtp']);
@@ -11,33 +12,23 @@ Route::post('/auth/verify-otp', [AuthController::class, 'verifyOtp']);
 Route::post('/auth/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 Route::middleware('auth:sanctum')->group(function () {
-
-    // --------------------------------------------------------
-    // F1 - CRUD des pots
-    // --------------------------------------------------------
     Route::apiResource('pots', PotController::class);
 
-    // --------------------------------------------------------
-    // F2 - Gestion des membres
-    // --------------------------------------------------------
     Route::get('/pots/{pot_id}/membres', [MembreController::class, 'index']);
     Route::post('/membres', [MembreController::class, 'store']);
     Route::get('/membres/{membre}', [MembreController::class, 'show']);
     Route::put('/membres/{membre}', [MembreController::class, 'update']);
     Route::delete('/membres/{membre}', [MembreController::class, 'destroy']);
 
-    // --------------------------------------------------------
-    // F3 - Gestion des documents (CNI recto/verso)
-    // --------------------------------------------------------
     Route::post('/membres/{membre}/documents', [MembreController::class, 'addDocument']);
     Route::delete('/documents/{document}', [MembreController::class, 'deleteDocument']);
 
-    // --------------------------------------------------------
-    // F4 - Gestion des paiements (cotisations)
-    // --------------------------------------------------------
     Route::post('/paiements/generer-lien', [CotisationController::class, 'genererLien']);
     Route::post('/paiements/confirmer', [CotisationController::class, 'confirmerPaiement']);
     Route::post('/paiements/saisie-manuelle', [CotisationController::class, 'saisieManuelle']);
     Route::get('/pots/{pot_id}/cotisations', [CotisationController::class, 'historique']);
 
+    Route::get('/registre', [RegistreController::class, 'index']);
+    Route::get('/registre/verifier', [RegistreController::class, 'verifierIntegrite']);
+    Route::get('/registre/exporter', [RegistreController::class, 'exporter']);
 });
